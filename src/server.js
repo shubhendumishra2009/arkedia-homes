5509ab70e3afecaeafb8cd5db82cbd0f85990edc
shubhendumishra2009 <shubhendumishra2009@gmail.com>
2025-04-13 00:49:36 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ async function startServer() {
     }
     
     // Start Express server
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
